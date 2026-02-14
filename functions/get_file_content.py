27ec2208e640dda95b7087e6c8cd0369be3ca01a
ebagabee <gh.alvesantos@gmail.com>
2026-02-14 11:41:35 -0300

    feat: valid if the file is a file or not and is the same directory

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,15 @@
+import os
+
 def get_file_content(working_directory, file_path):
-    pass
+    base = os.path.abspath(working_directory)
+    target = os.path.abspath(file_path)
+
+    same_dir = os.path.commonpath([base]) == os.path.commonpath([base, target])
+
+    if not same_dir:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+    is_file = os.path.isfile(file_path)
+
+    if not is_file:
+        return f'Error: File not found or is not a regular file: "{file_path}"'
